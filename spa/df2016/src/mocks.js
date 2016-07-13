9116e87573e6b975679662b7ff4236a6651f48b7
Dan Shahin <dan@codescience.com>
2016-07-12 18:20:37 -0700

    add page variables

--- spa/df2016/src/mocks.js
+++ spa/df2016/src/mocks.js
@@ -3,43 +3,43 @@
 
 window.configSettings = {
     staticPath: '',
-    config: {  },
+    user:{
+        id : faker.random.uuid(),
+        firstName : faker.name.firstName(),
+        lastName : faker.name.lastName(),
+        uiThemeDisplayed : 'Theme3',
+    },
     remoteActions: {
-        helloWorld              : '{!$RemoteAction.DF2016Controller.helloWorld}',
+        helloWorld  : '{!$RemoteAction.DF2016Controller.helloWorld}',
     },
     mocks: {
-
-        '{!$RemoteAction.DF2016Controller.helloWorld}': {
-            timeout : 250,
-            products : [],
-            error : false,
-            numberOfTiles : faker.random.number(20),
-            method : function(){
-                if(this.products.length === 0){
-                    for(var i=0; i< this.numberOfTiles ;i++){
-                        this.products.push({
-                            image: faker.image.image(),
-                            title: faker.lorem.sentence(),
-                            motto: faker.company.catchPhrase(),
-                            price: faker.commerce.price(99.99),
-                            summary :faker.lorem.paragraph()
-                        });
-                    }
-                }
-                return this.products;
-            }
-        }
+        '{!$RemoteAction.DF2016Controller.helloWorld}': helloWorld()
     }
 };
 
-
-var hellowWorld = function(){
+//mock factory
+function helloWorld(){
     return {
-        timeout : 5000,
+        timeout : 250,
+        products : [],
+        error : false,
+        numberOfTiles : faker.random.number(30),
         method : function(){
-            return 'hello';
+            if(this.products.length === 0){
+                for(var i=0; i< this.numberOfTiles ;i++){
+                    this.products.push({
+                        id : faker.random.uuid(),
+                        image: faker.image.image(),
+                        title: faker.lorem.sentence(),
+                        motto: faker.company.catchPhrase(),
+                        price: faker.commerce.price(99.99),
+                        summary :faker.lorem.paragraph()
+                    });
+                }
+            }
+            return this.products;
         }
-    }
+    };
 }
 
 
