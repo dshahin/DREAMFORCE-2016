9dba47c5cce89115d303588b92bbe8ce86960261
Dan Shahin <dan@codescience.com>
2016-09-03 21:04:27 -0700

    add jsr-mocks as npm module

--- spa/df2016/src/app.config.js
+++ spa/df2016/src/app.config.js
@@ -3,5 +3,5 @@ routing.$inject = ['$urlRouterProvider', '$locationProvider', 'jsrMocksProvider'
 export default function routing($urlRouterProvider, $locationProvider, jsrMocksProvider) {
   $locationProvider.html5Mode(false);
   $urlRouterProvider.otherwise('/');
-  jsrMocksProvider.setMocks(configSettings.mocks);
+  jsrMocksProvider.setMocks(window.configSettings.mocks);
 }
